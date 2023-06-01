14442bb0a2aa6fa07b1f4dba395f39005b99345e
pangorithm <phs559800@gmail.com>
2023-06-01 17:13:46 +0900

    1번 문자 개수 세기

--- app/src/main/java/com/coding/study/chapter01/p01countDuplicateCharacter/test.java
+++ app/src/main/java/com/coding/study/chapter01/p01countDuplicateCharacter/test.java
@@ -1,5 +1,31 @@
 package com.coding.study.chapter01.p01countDuplicateCharacter;
 
-public class test {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class test {
+  public static void main(String[] args) {
+    Scanner scanner = new java.util.Scanner(System.in);
+    System.out.println("문자열을 입력하세요");
+    String str = scanner.nextLine();
+    Map<Character, Integer> countresult = countDuplicateCharacters(str);
+
+    System.out.println("문자열의 각 문자의 개수");
+    countresult.forEach((key, value) -> System.out.printf("%c : %d\n", key, value));
+
+    scanner.close();
+  }
+
+  static Map<Character, Integer> countDuplicateCharacters(String str) {
+    Map<Character, Integer> result = new HashMap<>();
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+
+      result.compute(ch, (key, value) -> (value == null) ? 1 : ++value);
+    }
+
+    return result;
+  }
 }
