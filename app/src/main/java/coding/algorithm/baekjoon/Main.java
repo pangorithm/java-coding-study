a103a6d75890c43a9b9e8fde097edb11c63f2ec1
pangorithm <phs559800@gmail.com>
2023-08-28 17:33:21 +0900

    백준 입출력 기본 양식 업데이트

--- app/src/main/java/coding/algorithm/baekjoon/Main.java
+++ app/src/main/java/coding/algorithm/baekjoon/Main.java
@@ -1,20 +1,19 @@
 package coding.algorithm.baekjoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
   public static void main(String[] str) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    ////////////////////////////////////////////////////////////////////////////
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
 
 
-    ////////////////////////////////////////////////////////////////////////////
-    br.close();
+
+    bw.write(n + "\n");
     bw.close();
+    br.close();
   }
 }
